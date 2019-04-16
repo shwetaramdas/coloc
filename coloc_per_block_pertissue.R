dcd9713368b3a35a3e1508057e7b579138ddc574
Shweta <shwetaramdas@users.noreply.github.com>
2019-04-16 11:00:15 -0400

    Add files via upload

---
+++ coloc_per_block_pertissue.R
@@ -0,0 +1,104 @@
+library(coloc)
+library(qvalue)
+library(dplyr)
+library(readr)
+library(tidyr)
+
+args = commandArgs(trailingOnly=TRUE)
+print(args)
+chr = args[1]
+pos = args[2]
+sentinel = args[3]
+
+p1 = as.numeric(args[4])
+p2 = as.numeric(args[5])
+tissue = args[6]
+ldblock = c(args[7], as.numeric(args[8]), as.numeric(args[9]))
+
+print(ldblock)
+print(chr)
+pos = as.numeric(pos)
+
+allps = read.table(paste0("/home/shwetar/glgc/analysis_april2/ldl/",tissue,"/t1.txt"),stringsAsFactors=F)
+qs = qvalue(allps[,7])
+p12 = 1 - qs$pi0
+print(p12)
+
+
+#gene coordinates
+geneids = read_delim("/home/shwetar/t2d/snps/gencode_geneids.txt",comment="#",delim="\t",col_names=FALSE)
+
+gwas = read_delim("/home/shwetar/glgc/data/hg38/forcoloc_metal_ldl.txt",delim="\t")
+gwas$SNP = gsub(":","_",gwas$SNP)
+
+#find genes within 1Mb
+cisgenes = geneids %>% filter(X1 == chr) %>% filter(((X2 > pos - 1000000) & (X2 < pos + 1000000)) | ((X3 > pos - 1000000) & (X3 < pos + 1000000)) | ((X2 <  pos - 1000000) & (X3 > pos + 1000000))) %>% select(X4) %>% distinct()
+
+print(cisgenes)
+
+#remove the version number from the ensembl gene id
+cisgenes$X4 = gsub("\\..*","",cisgenes$X4)
+
+#keep only those egenes that are also cis to the sentinel snp
+egenes = read.table(gzfile(paste0("/project/chrbrolab/gtex/results/GTEx_Analysis_v8_eQTL/GTEx_Analysis_v8_eQTL/",tissue,".v8.egenes.txt.gz")), sep="\t",header=T)
+egenes[,1] = as.character(egenes[,1])
+egenes = as.tbl(egenes)
+egenes = egenes %>% filter(qval < 0.05) %>% select(gene_id) %>% distinct()
+egenes$gene_id = gsub("\\..*","", egenes$gene_id)
+egenes = inner_join(egenes, cisgenes, by=c('gene_id'='X4'))
+
+print(egenes)
+
+
+#subset gwas data to within ld block
+
+coloc_results = tibble(chr="", start="", end="", snp = "", gene="", pval1 = "", pval2 = "", pval3="",pval4="")
+
+#get sample size for eqtl
+command = paste0("cat /project/chrbrolab/gtex/results/GTEx_Analysis_v8_eQTL//GTEx_Analysis_v8_eQTL_covariates/",tissue,".v8.covariates.txt | awk '{print NF-1}' | head -1")
+
+num_samples_expn = as.numeric(system(command, intern=TRUE))
+
+
+for(gene in egenes$gene_id){
+        print(gene)
+        #read in eqtl data for egenes
+        files = list.files(paste0("/project/chrbrolab/gtex/results//GTEx_Analysis_v8_eQTL/GTEx_Analysis_v8_eQTL_all_associations_per_gene/",tissue))
+        infile = paste0("/project/chrbrolab/gtex/results//GTEx_Analysis_v8_eQTL/GTEx_Analysis_v8_eQTL_all_associations_per_gene/",tissue,"/" ,grep(gene, files,value=TRUE)[2])
+        eqtl = read_delim(infile,delim="\t")
+        eqtl$variant = gsub("_[A-Z]*_[A-Z]*_b38","",eqtl$variant_id)
+
+        #merge with gwas data
+        m = inner_join(gwas, eqtl, by=c("SNP" = "variant"))
+        print(nrow(m))
+        dataset1 = list()
+        dataset2 = list()
+
+        dataset1$pvalues = m$Pvalue
+        dataset1$N = m$N[1]
+
+        dataset1$MAF = pmin(m$EAF, 1 - m$EAF)
+        dataset1$beta = m$Beta
+        dataset1$varbeta = m$SE * m$SE
+        dataset1$type = "quant"
+#        dataset1$s = 74124/824006
+
+        dataset2$pvalues = m$pval_nominal
+        dataset2$N = num_samples_expn
+        dataset2$MAF = m$maf
+        dataset2$beta = m$slope
+        dataset2$varbeta = m$slope_se * m$slope_se
+        dataset2$type = "quant"
+
+        #run coloc
+
+        if(nrow(m) == 0){
+                next;
+        }
+#       COLOC = coloc.abf(dataset1, dataset2, p1=p1, p2=p2,p12=p12)
+        COLOC = coloc.abf(dataset1, dataset2)
+        coloc_results = add_row(coloc_results, chr=chr, start=as.numeric(ldblock[2]), end = as.numeric(ldblock[3]), snp = sentinel, gene = gene, pval1 = COLOC$summary[3], pval2 = COLOC$summary[4], pval3 = COLOC$summary[5],pval4 = COLOC$summary[6])
+
+}
+
+write.table(coloc_results, file=paste0("/home/shwetar/glgc/analysis_april2/ldl/",tissue,"/egenes_noparameters_coloc_",chr,"_",pos,".txt"),row.names=F,col.names=F,sep="\t",quote=F)
---
+++ runcoloc_pertissue.R
@@ -0,0 +1,85 @@
+library(tidyr)
+library(readr)
+library(dplyr)
+library(coloc)
+library(qvalue)
+
+overlap = function(chr, start, end){
+#       print(chr)
+#       print(start)
+#       print(end)
+        toreturn = sentinel %>% filter(X2 == chr) %>% filter((X3 >= start) & (X3 <= end))
+        return(nrow(toreturn))
+}
+
+args = commandArgs(trailing=TRUE)
+tissue = args[1]
+p2 = as.numeric(args[2])
+
+
+#LD blocks from Joe Pickrell
+ld = read_delim("/home/shwetar/t2d/snps/ld_hg38.bed", col_names=FALSE,delim="\t")
+
+#List of all GWAS SNPs with Pvalue < 5e-08
+sentinel = read_delim("/home/shwetar/glgc/analysis_april2/ldl/sentinelsnps.txt",delim="\t",col_names=FALSE)
+
+block_in_sentinel = apply(ld, 1, function(x){overlap(x[1],as.numeric(x[2]),as.numeric(x[3]));})
+
+p1 = length(which(block_in_sentinel > 0)) / length(block_in_sentinel)
+print(p1)
+
+blocks_with_sentinel = which(block_in_sentinel > 0)
+
+#find sentinel snp in each block
+chrs = c()
+starts = c()
+sentinelsperblock = c()
+
+
+for(i in blocks_with_sentinel){
+	ldblock = ld[i,]
+	sentinels = sentinel %>% filter(X2 == as.character(ldblock[1])) %>% filter(X3 >= as.numeric(ldblock[2])) %>% filter(X3 <= as.numeric(ldblock[3])) %>%  arrange(X9)
+	chrs = c(chrs, as.character(sentinels[1,2]))
+	starts = c(starts, as.numeric(sentinels[1,3]))
+	sentinelsperblock = c(sentinelsperblock, paste0(as.character(sentinels[1,2]), "_", as.character(sentinels[1,3]),"_"))
+}
+
+write.table(sentinelsperblock, file="/home/shwetar/glgc/analysis_april2/ldl/sentinelsperblock.txt",row.names=F, col.names=F,quote=F)
+
+#p12
+#for each sentinel snp, read in all tests, and compute q value
+
+#gene coordinates
+geneids = read_delim("~/t2d/snps/gencode_geneids.txt",comment="#",delim="\t",col_names=FALSE)
+
+
+#From all GTEx results, extract only those SNP_gene pairs of which the SNP is found in the sentinels per block
+command = paste0("zcat /project/chrbrolab/gtex/results/GTEx_Analysis_v8_eQTL/GTEx_Analysis_v8_eQTL_all_associations/",tissue,".allpairs.txt.gz | grep -f /home/shwetar/glgc/analysis_april2/ldl/sentinelsperblock.txt > /home/shwetar/glgc/analysis_april2/ldl/",tissue,"/t1.txt")
+system(command)
+allps = read.table(paste0("/home/shwetar/glgc/analysis_april2/ldl/",tissue,"/t1.txt"),stringsAsFactors=F)
+qs = qvalue(allps[,7])
+p12 = 1 - qs$pi0
+print(p12)
+
+
+#read in gwasdata
+gwas = read_delim("/home/shwetar/glgc/data/hg38/forcoloc_metal_ldl.txt",delim="\t")
+gwas$SNP = gsub(":","_",gwas$SNP)
+
+
+#now submit coloc analyses for each ld block
+j = 1
+
+coloc_results = tibble(chr="", start="", end="", snp = "", gene="", pval1 = "", pval2 = "", pval3="",pval4="")
+for(i in blocks_with_sentinel){
+	print(paste0("Starting analysis for LD block ", i))
+	ldblock = ld[i,]
+	sentinel = sentinelsperblock[j]
+	chr = strsplit(sentinel, "_")[[1]][1]
+	pos = as.numeric(strsplit(sentinel, "_")[[1]][2])
+
+	command = paste0("bsub -M 40000 -J ", chr, ":", pos, " -o /home/shwetar/glgc/analysis_april2/ldl/",tissue,"/error",chr,"_",pos, ".o -e /home/shwetar/glgc/analysis_april2/ldl/",tissue,"/error",chr,"_",pos, ".e Rscript /home/shwetar/glgc/analysis_april2/ldl/",tissue,"/coloc_per_block_pertissue.R ", chr, " ", pos, " ", sentinel, " ", p1, " ", p2, " ", tissue, " ", as.character(ldblock[1,1]), " ", as.numeric(ldblock[1,2]), " ", as.numeric(ldblock[1,3]))
+	print(command)
+	system(command)
+	j = j + 1
+}
